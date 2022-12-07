2afe11bdec968d14df5b21bbc36935fff3936db1
Abdullah Alghamdi <61493052+xlizro@users.noreply.github.com>
2022-12-07 13:14:35 +0300

    done

--- js/app.js
+++ js/app.js
@@ -12,164 +12,166 @@
 
 
  *------------------------------------------------------------------------**/
+window.addEventListener('DOMContentLoaded', function() {
+
+    // Global variables
+    let ul = document.getElementById('navbar__list');
+    let sections = document.querySelectorAll('section');
+    let fragment = document.createDocumentFragment();
+    let main = document.querySelector('main');
+    let h2Section = document.querySelectorAll('h2');
+
+    /*==== END OF SECTION ====*/
 
 
-// Global variables
-let ul = document.getElementById('navbar__list');
-let sections = document.querySelectorAll('section');
-let fragment = document.createDocumentFragment();
-let main = document.querySelector('main');
-let h2Section = document.querySelectorAll('h2');
+    // Building the navigation bar
+    function navbar() {
+        for (let i = 0; i < sections.length; i++) {
+            // creating li
+            let li = document.createElement('li');
+            // creating a
+            let a = document.createElement('a');
+            // adding text to a
+            a.textContent = sections[i].getAttribute('data-nav');
+            // adding class to a
+            a.classList.add('menu__link');
+            // adding href to a
+            a.setAttribute('href', '#' + sections[i].getAttribute('id'));
+            // adding a to li
+            li.appendChild(a);
 
-/*==== END OF SECTION ====*/
+            fragment.appendChild(li);
+            // updating section header
+            h2Section[i].innerHTML = sections[i].getAttribute('data-nav');
 
-
-// Building the navigation bar
-function navbar() {
-    for (let i = 0; i < sections.length; i++) {
-        // creating li
-        let li = document.createElement('li');
-        // creating a
-        let a = document.createElement('a');
-        // adding text to a
-        a.textContent = sections[i].getAttribute('data-nav');
-        // adding class to a
-        a.classList.add('menu__link');
-        // adding href to a
-        a.setAttribute('href', '#' + sections[i].getAttribute('id'));
-        // adding a to li
-        li.appendChild(a);
-
-        fragment.appendChild(li);
-        // updating section header
-        h2Section[i].innerHTML = sections[i].getAttribute('data-nav');
-
+        }
+        // adding li to ul
+        ul.appendChild(fragment);
     }
-    // adding li to ul
-    ul.appendChild(fragment);
-}
-// calling the function navbar
-navbar();
+    // calling the function navbar
+    navbar();
 
-/*==== END OF SECTION ====*/
+    /*==== END OF SECTION ====*/
 
 
 
-// Adding class 'active' to section if it is in the viewport
+    // Adding class 'active' to section if it is in the viewport
 
-function active() {
-    // getting the height of the viewport and the top of the viewport
-    for (let i = 0; i < sections.length; i++) {
-        // getting the position of the section
-        let rect = sections[i].getBoundingClientRect();
-        // check if the section is in the viewport 
-        if (rect.top >= 0 && rect.top <= 300) {
-            // adding class 'your-active-class' to the section
-            sections[i].classList.add('your-active-class');
-            // getting the link of the section
-            let link = document.querySelector('a[href="#' + sections[i].getAttribute('id') + '"]');
-            // adding class 'active' to the link
-            link.classList.add('active');
-        } else {
-            // removing class 'your-active-class' from the section
-            sections[i].classList.remove('your-active-class');
-            // getting the link of the section
-            let link = document.querySelector('a[href="#' + sections[i].getAttribute('id') + '"]');
-            // removing class 'active' from the link
-            link.classList.remove('active');
+    function active() {
+        // getting the height of the viewport and the top of the viewport
+        for (let i = 0; i < sections.length; i++) {
+            // getting the position of the section
+            let rect = sections[i].getBoundingClientRect();
+            // check if the section is in the viewport 
+            if (rect.top >= 0 && rect.top <= 300) {
+                // adding class 'your-active-class' to the section
+                sections[i].classList.add('your-active-class');
+                // getting the link of the section
+                let link = document.querySelector('a[href="#' + sections[i].getAttribute('id') + '"]');
+                // adding class 'active' to the link
+                link.classList.add('active');
+            } else {
+                // removing class 'your-active-class' from the section
+                sections[i].classList.remove('your-active-class');
+                // getting the link of the section
+                let link = document.querySelector('a[href="#' + sections[i].getAttribute('id') + '"]');
+                // removing class 'active' from the link
+                link.classList.remove('active');
+            }
         }
     }
-}
-// calling the function active to check if the section is in the viewport
-window.addEventListener('scroll', active);
+    // calling the function active to check if the section is in the viewport
+    window.addEventListener('scroll', active);
 
-/*==== END OF SECTION ====*/
-
+    /*==== END OF SECTION ====*/
 
 
-// scrolling to the section when clicking on the navigation bar
-function smoothScroll() {
-    // getting all the links    
-    let links = document.querySelectorAll('a');
-    // looping over the links
-    for (let i = 0; i < links.length; i++) {
-        // adding event listener to each link
-        links[i].addEventListener('click', function(e) {
-            // preventing the default behavior which is to jumping to the section
-            e.preventDefault();
-            // getting the href attribute of the link
-            let section = document.querySelector(links[i].getAttribute('href'));
-            // scrolling to the section smoothly
-            section.scrollIntoView({
-                behavior: 'smooth'
+
+    // scrolling to the section when clicking on the navigation bar
+    function smoothScroll() {
+        // getting all the links    
+        let links = document.querySelectorAll('a');
+        // looping over the links
+        for (let i = 0; i < links.length; i++) {
+            // adding event listener to each link
+            links[i].addEventListener('click', function(e) {
+                // preventing the default behavior which is to jumping to the section
+                e.preventDefault();
+                // getting the href attribute of the link
+                let section = document.querySelector(links[i].getAttribute('href'));
+                // scrolling to the section smoothly
+                section.scrollIntoView({
+                    behavior: 'smooth'
+                });
             });
-        });
+        }
     }
-}
-// calling the function smoothScroll
-smoothScroll();
+    // calling the function smoothScroll
+    smoothScroll();
 
-/*==== END OF SECTION ====*/
-
+    /*==== END OF SECTION ====*/
 
 
-// Hiding the fixed navigation bar while not scrolling
-let Timer;
-// add event listener to the window
-window.addEventListener('scroll', function() {
-    // show the navigation bar
-    ul.style.display = 'block';
-    // clear the Timer if it has already been set
-    clearTimeout(Timer);
-    // set the Timer
-    Timer = setTimeout(function() {
-        // hide the navigation bar
-        ul.style.display = 'none';
-    }, 4000); // hiding the navigation bar after 3 seconds
-});
 
-/*==== END OF SECTION ====*/
-
-//* 1- Adding a scroll to top button on the page that’s only visible when the user scrolls below the fold of the page.
-
-// create the button 
-let btn = document.createElement('a');
-// add text to the button
-btn.textContent = '⬆';
-// add class to the button
-btn.classList.add('top');
-// add style to the button  S
-btn.style.cssText = 'padding: 10px; color: black; font-size: 20px; border-radius:3rem; position: fixed; bottom: 180px; left: 1.5em; display: none; z-index: 1000; cursor:pointer; background-color: #fff; ';
-// add the button to the main
-main.appendChild(btn);
-
-//* 2- add event listener to the button to scroll to the top of the page
-
-btn.addEventListener('click', function() {
-    // scroll to the top of the page
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
+    // Hiding the fixed navigation bar while not scrolling
+    let Timer;
+    // add event listener to the window
+    window.addEventListener('scroll', function() {
+        // show the navigation bar
+        ul.style.display = 'block';
+        // clear the Timer if it has already been set
+        clearTimeout(Timer);
+        // set the Timer
+        Timer = setTimeout(function() {
+            // hide the navigation bar
+            ul.style.display = 'none';
+        }, 4000); // hiding the navigation bar after 3 seconds
     });
-});
 
-//* 3- show the button when the user scrolls below the fold of the page
+    /*==== END OF SECTION ====*/
 
-window.addEventListener('scroll', function() {
-    // get the height of the viewport
-    let height = window.innerHeight;
+    //* 1- Adding a scroll to top button on the page that’s only visible when the user scrolls below the fold of the page.
 
-    // get the position of the top of the page
-    let top = window.pageYOffset || document.documentElement.scrollTop;
+    // create the button 
+    let btn = document.createElement('a');
+    // add text to the button
+    btn.textContent = '⬆';
+    // add class to the button
+    btn.classList.add('top');
+    // add style to the button  S
+    btn.style.cssText = 'padding: 10px; color: black; font-size: 20px; border-radius:3rem; position: fixed; bottom: 180px; left: 1.5em; display: none; z-index: 1000; cursor:pointer; background-color: #fff; ';
+    // add the button to the main
+    main.appendChild(btn);
 
-    // check if the user scrolls below the fold of the page
-    if (top > height) {
-        // show the button
-        btn.style.display = 'block';
-    } else {
-        // hide the button
-        btn.style.display = 'none';
-    }
+    //* 2- add event listener to the button to scroll to the top of the page
+
+    btn.addEventListener('click', function() {
+        // scroll to the top of the page
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+
+    //* 3- show the button when the user scrolls below the fold of the page
+
+    window.addEventListener('scroll', function() {
+        // get the height of the viewport
+        let height = window.innerHeight;
+
+        // get the position of the top of the page
+        let top = window.pageYOffset || document.documentElement.scrollTop;
+
+        // check if the user scrolls below the fold of the page
+        if (top > height) {
+            // show the button
+            btn.style.display = 'block';
+        } else {
+            // hide the button
+            btn.style.display = 'none';
+        }
+    });
+
 });
 
 /*==== END OF SECTION ====*/
